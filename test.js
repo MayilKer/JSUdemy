975c837abad376bd0d4096bb03a7f0034b3e09ab
Mayil <kerimov0410@gmail.com>
2022-09-04 15:38:07 +0400

    First Commit

---
+++ test.js
@@ -0,0 +1,112 @@
+"use strict";
+
+// a = 5;
+
+// console.log(a);
+
+let number = 10;
+console.log(number);
+
+let num = [1,2,3];
+console.log(num[1]);
+
+const numberConst = 22;
+
+number = 20;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+let obj2 = {
+    Name: 'Mayil',
+    Surname: 'Kerimov',
+    IsMarried: false
+};
+
+console.log(obj2.IsMarried);
+
+console.log(obj);
+
+obj.a = 20;
+console.log(obj);
+
+// console.log(names);
+// var names = 'Mayil'; 
+// var hoisting
+let surname = 'Zeynalov';
+{
+    var sur = 'Kerimov';
+    surname = sur;
+}
+
+console.log(sur);
+console.log(surname);
+
+// {
+//     let surs = 'Kerimov';
+// }
+// console.log(surs);
+
+let storeDescription = {
+    budget:10000,
+    employees:['Kerumov','Sadikhan','Aliyev'],
+     products : {
+        xleb:4,
+        lod:10
+    },
+    open:true
+};
+
+console.log(storeDescription.employees[1]);
+
+
+let objnew = {
+    '1': 'Kerimov',
+    b: 3,
+    c: 1,
+    2:'Hello'
+
+};
+
+objnew.a = "Load";
+
+objnew[5] = 'Mayil';
+
+
+
+console.log(objnew);
+console.log(objnew.a);
+// console.log(objnew['b']);
+// Better dot notation
+console.log(objnew[1]);
+console.log(objnew[2]);
+
+const syb = Symbol("Id");
+
+console.log(syb.toString());
+
+// const result = confirm("Are you here?");
+
+// const answer = +prompt("Вам есть 18?");
+
+// if(answer < 18){
+//     alert('Warning');
+// }
+
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('Adivizi daxil edin', '');
+// answers[1] = prompt('Soyadivizi daxil edin', '');
+// answers[2] = prompt('Yasivizi daxil edin', '');
+
+// document.write(answers);
+// document.close();
+
+let nums = 1;
+
+console.log(nums++);
+console.log(nums);
