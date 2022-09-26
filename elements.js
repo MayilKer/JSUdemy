621cfb196d0c22f7377bee2f28aee5ed3cf19f2f
Mayil Kerimov <kerimov0410@gmail.com>
2022-09-26 18:56:20 +0400

    PcCommit

---
+++ elements.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const nav = document.getElementById('nav');
+const boxes = document.getElementsByName('boxes');
+const boxColl = document.getElementsByClassName('box');
+const buttonColl = document.getElementsByTagName('button');
+const navNode = document.querySelector('#nav');
+const boxNode = document.querySelectorAll('.box');
+const divNode = document.querySelectorAll('div');
+
+
+
+
+boxes.forEach(a => {
+    console.log(a);
+});
+
+
+
+console.log(nav);
+console.log(boxes);
+console.log(boxColl);
+console.log(buttonColl);
+console.log(navNode);
+console.log(boxNode);
+console.log(divNode);
---
+++ intrview.js
@@ -0,0 +1,11 @@
+"strict mode";
+
+console.log([ ] + false - null + true);
+let y = 1;
+let x = y = 2;
+ console.log(typeof([] + 1 + 2));
+ console.log(null || 2 && 3 || 4);
+
+let a = [1];
+let b = [1];
+console.log(a == b);
