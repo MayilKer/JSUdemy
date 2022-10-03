541d1987c13a18bfe9c105c9203c4434bd9d9070
Mayil Kerimov <kerimov0410@gmail.com>
2022-10-03 13:57:59 +0400

    Lesson 43

--- elements.js
+++ elements.js
@@ -7,6 +7,8 @@ const buttonColl = document.getElementsByTagName('button');
 const navNode = document.querySelector('#nav');
 const boxNode = document.querySelectorAll('.box');
 const divNode = document.querySelectorAll('div');
+const wrapper = document.getElementById('wrapper');
+const hearts = document.querySelectorAll('.heart');
 
 
 
@@ -27,3 +29,44 @@ console.log(divNode);
 
 nav.style.backgroundColor = 'green';
 boxColl[0].style.cssText = 'color: red; width: 200px; background-color: grey;';
+
+hearts.forEach(element => {
+    element.style.backgroundColor = 'red';
+});
+
+// document.body.append(document.createTextNode('hELLO World'));
+
+const div = document.createElement('div');
+div.classList.add('black');
+document.body.append(div);
+nav.appendChild(div);
+// document.body.prepend(div);
+
+// document.querySelector('.heart').append(div);
+
+const text = document.createTextNode('Hello World');
+
+const sibling = document.createElement('div');
+
+sibling.style.width = '100px';
+sibling.style.height = '200px';
+sibling.style.backgroundColor = 'red';
+sibling.style.display = 'flex';
+sibling.style.alignItems = 'center';
+sibling.style.justifyContent = 'center';
+
+// hearts[0].before(sibling);
+// hearts[1].after(text);
+// hearts[1].after(sibling);
+// hearts[2].remove();
+// hearts[0].replaceWith(boxes[0]);
+
+wrapper.insertBefore(sibling,hearts[2]);
+wrapper.removeChild(hearts[2]);
+wrapper.replaceChild(boxes[0],hearts[0]);
+
+sibling.textContent = 'Hello Mayil';
+
+sibling.innerHTML = '<p>How are you?</p>';
+
+sibling.insertAdjacentHTML('beforeend','<h2>Olaaa</h2>');
