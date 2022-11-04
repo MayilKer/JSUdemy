405dc2d4a2f76e95e7c2d6db8253408da15e30f0
Mayil Kerimov <kerimov0410@gmail.com>
2022-11-05 03:31:11 +0400

    thisPart2

--- this.js
+++ this.js
@@ -2,9 +2,49 @@
 
 
 
-// with strict mode this function will show undefined.
+// 1)with strict mode this function will show undefined, but without strict mode input will be - Window.
 function CallWindow(){
     console.log(this);
 }
 
 CallWindow();
+///
+
+//2)Will execute object - objectThis
+const objThis = {
+    a: 2,
+    b: 3,
+    thisFunc: function(){
+        console.log(this);
+    }
+};
+
+objThis.thisFunc();
+///
+
+
+
+//3) this в конструкторах и классах - это новый экземпляр объекта
+
+function User(name, id){
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        return `${this.name} says Hello`;
+    };
+}
+
+const newUser = new User('Mayil', 23);
+///
+
+function aboutYourself(surname){
+    console.log(`${this.name} ${surname}`);
+}
+
+const per = {
+    name: 'Matilda'
+};
+
+aboutYourself.apply(per,['Semonovna']);
+aboutYourself.call(per,'Kerimova');
